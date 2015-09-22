1df071aa0c45d11003590f95269059a76514c804
lijing.lj <lijing.lj@alibaba-inc.com>
2015-09-22 17:15:13 +0800

    Add ProgressBar Demo

---
+++ GLViewDemos/src/com/rendernode/test/demos/ProgressBarActivity.java
@@ -0,0 +1,15 @@
+package com.rendernode.test.demos;
+
+import android.os.Bundle;
+
+import com.rendernode.test.R;
+
+public class ProgressBarActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setGLContentView(R.layout.gl_layout_progress_bar);
+	}
+}
